9edd934acf6de6229fe7ea30f716c03ef00113e5
Coleman Criner <colemanccriner@gmail.com>
2023-05-13 10:46:00 -0700

    DRIVER.py is set up for NE/NEB Catalog Reports and MP Listing Counts Report tasks to be completed

---
+++ DRIVER.py
@@ -0,0 +1,15 @@
+import nCatalog
+import listingCounts
+
+# Newegg & Newegg Business Catalog Reports
+nCatalog.ne_report()
+nCatalog.neb_report()
+
+# Marketplace Listing Counts Report
+listingCounts.report()
+
+
+
+
+
+
